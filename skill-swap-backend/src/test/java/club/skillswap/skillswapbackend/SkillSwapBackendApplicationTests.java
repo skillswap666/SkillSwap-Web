1dc4532f017afafed999ae67cda964c28722f42f
LiChenyang-GZ <1169281314@qq.com>
2025-09-24 14:01:44 +1000

    upload backend

---
+++ skill-swap-backend/src/test/java/club/skillswap/skillswapbackend/SkillSwapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package club.skillswap.skillswapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSwapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
