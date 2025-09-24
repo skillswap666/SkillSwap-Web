92c1495f2a1057339f2ad7e13bc6cfe8ee179f90
LiChenyang-GZ <1169281314@qq.com>
2025-09-24 21:06:29 +1000

    modify user profile

---
+++ skill-swap-backend/src/main/java/club/skillswap/skillswapbackend/dto/UpdateProfileRequestDto.java
@@ -0,0 +1,12 @@
+package club.skillswap.skillswapbackend.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateProfileRequestDto {
+    // 前端可以只发送他们想更新的字段
+    private String username;
+    private String avatarUrl;
+    private String bio;
+    // 我们之后还可以添加技能列表等
+}
